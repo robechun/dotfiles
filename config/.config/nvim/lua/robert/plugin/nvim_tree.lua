51e3f0d5f22a45a927815278268ab6bd6708ee47
Robert Chung <robertchung97@gmail.com>
2021-05-16 17:25:19 -0700

    fix Nvim tree command

--- config/.config/nvim/lua/robert/plugin/nvim_tree.lua
+++ config/.config/nvim/lua/robert/plugin/nvim_tree.lua
@@ -1,9 +1,7 @@
--- vim.g.nvim_tree_ignore = {'.DS_Store', 'tags', '.idea', '.sass-cache', '.git', 'node_modules', '.cache' }
+vim.g.nvim_tree_ignore = {'.DS_Store', 'tags', '.idea', '.sass-cache', '.git', 'node_modules', '.cache' }
 vim.g.nvim_tree_gitignore = 1 -- 0 by default
 vim.g.nvim_tree_indent_markers = 1 -- 0 by default, this option shows indent markers when folders are open
-vim.g.nvim_tree_follow = 1 -- 0 by default, this option allows the cursor to be updated when entering a buffer
 
-
-vim.api.nvim_set_keymap('n', '<leader>.', 'NvimTreeToggle<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>m', 'NvimTreeFindFile<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>.', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>m', ':NvimTreeFindFile<CR>', {noremap = true, silent = true})
 
