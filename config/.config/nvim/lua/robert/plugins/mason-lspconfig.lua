4a6ba9987432b3b786cc1284a5e8e93accca5153
robechun <robertchung97@gmail.com>
2025-06-27 22:08:01 -0700

    One more; get ahead of deprecation

--- config/.config/nvim/lua/robert/plugins/mason-lspconfig.lua
+++ config/.config/nvim/lua/robert/plugins/mason-lspconfig.lua
@@ -12,24 +12,9 @@ return {
 
         -----
         -- Set up diagnostics
-        local signs = {
-            { name = "DiagnosticSignError", text = "" },
-            { name = "DiagnosticSignWarn", text = "" },
-            { name = "DiagnosticSignHint", text = "" },
-            { name = "DiagnosticSignInfo", text = "" },
-        }
-
-        for _, sign in ipairs(signs) do
-            vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-        end
-
         local config = {
             -- disable virtual text
             virtual_text = true,
-            -- show signs
-            signs = {
-                active = signs,
-            },
             update_in_insert = true,
             underline = true,
             severity_sort = true,
@@ -41,6 +26,14 @@ return {
                 header = "",
                 prefix = "",
             },
+            signs = {
+                text = {
+                    [vim.diagnostic.severity.ERROR] = "",
+                    [vim.diagnostic.severity.WARN] = "",
+                    [vim.diagnostic.severity.HINT] = "",
+                    [vim.diagnostic.severity.INFO] = "",
+                },
+            }
         }
         vim.diagnostic.config(config)
 
